871b8d94813c764adc2f01a9a412a5d1e1207ebf
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-08-21 17:16:10 +0530

    first commit

---
+++ models/doctor.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const doctorSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    specialization: {
+        type: String,
+        enum: ['Cardiologist', 'Dermatologist', 'Pediatrician', 'Psychiatrist'],
+        required: true
+    },
+    experience: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    slots: {
+        type: Number,
+        required: true
+    },
+    fee: {
+        type: Number,
+        required: true
+    }
+});
+
+const DoctorModel = mongoose.model("Doctor", doctorSchema);
+
+module.exports = {
+    DoctorModel
+};
